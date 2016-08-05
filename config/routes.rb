a86577f9bc61ee07110ddf027eeeb4b6e6ff2b85
Adrian Yem <adrianyem91@outlook.com>
2016-08-05 13:09:20 +0800

    Half finished Day 5

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
    root to: 'landing#index'
    get :about, to: 'static_pages#about'
-   resources :topics
-   resources :posts
+   resources :topics  do
+     resources :posts
+   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
