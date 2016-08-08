6092034eb9b0f79f293a259c028bb68ae188d04b
Adrian Yem <adrianyem91@outlook.com>
2016-08-08 22:17:19 +0800

    Added in flash message and styling

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,4 +3,7 @@ class Post < ApplicationRecord
 
   has_many :comments
   belongs_to :topic
+
+  validates :title, length: {minimum: 5}, presence: true
+  validates :body, length: {minimum: 20}, presence: true
 end
